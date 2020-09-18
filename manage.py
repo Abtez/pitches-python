a222b69cc5afaf537d4e8303b8f198c960ad4441
Abzed <ibraabzed@gmail.com>
2020-09-18 17:29:42 +0300

    add db shell

--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 from app import create_app,db
 from flask_script import Manager,Server
 
-app = create_app(app)
+app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app,db=db)
 
 manager.add_command('server', Server)
 @manager.command
