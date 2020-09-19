5702fcb62fbcdb752e3a7fc981ee55783dc684cc
Abzed <ibraabzed@gmail.com>
2020-09-19 15:37:05 +0300

    add selectfield

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
 class ReviewForm(FlaskForm):
-    category = StringField('Pitch Category', validators=[Required()])
+    category = SelectField('Category', choices=[('Marketing','Marketing'),('Promotional','Promotional'),('Scholarship','Scholarship')],validators=[Required()])
     pitch = TextAreaField('Enter Pitch')
     submit = SubmitField('Submit')
     
