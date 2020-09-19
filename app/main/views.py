b06b248a061f64bb243e09d13a2f9d6f09269e17
Abzed <ibraabzed@gmail.com>
2020-09-19 13:27:06 +0300

    link with mdbootstrap

--- app/main/views.py
+++ app/main/views.py
@@ -3,15 +3,15 @@ from . import main
 from .forms import ReviewForm
 from .. import db
 
-@main.route('/',methods=['GET','POST'])
+@main.route('/')
 def index():
+   
+    return render_template('index.html')
+
+@main.route('/pitches/new_pitch', methods=['GET','POST'])
+def new_pitch():
     form = ReviewForm()
     if form.validate_on_submit():
         category = form.category.data
         pitch = form.pitch.data
-        return redirect(url_for('main.new_pitch'))
-    return render_template('index.html', review_form=form)
-
-@main.route('/pitches/new_pitch', methods=['GET','POST'])
-def new_pitch():
-    return render_template('new_pitch.html')
+    return render_template('new_pitch.html', review_form=form)
