693da4176fe049b2f39511741140b673fc0b948d
Abzed <ibraabzed@gmail.com>
2020-09-19 21:34:57 +0300

    register, login and logout users

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from ..models import User
 from .forms import RegistrationForm
 from .. import db
 from . import auth
-from flask_login import login_user
+from flask_login import login_user,logout_user,login_required
 from .forms import LoginForm,RegistrationForm
 
 @auth.route('/login',methods=['GET','POST'])
@@ -17,7 +17,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Pitch-Perfect Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -30,3 +30,9 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from . import db 
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model,UserMixin):
     __tablename__ = 'Users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255),unique=True,index=True)
