41a39894d65eddf5905d0ebaa7e1986cde781c59
Abzed <ibraabzed@gmail.com>
2020-09-19 13:07:02 +0300

    create new pitch route

--- app/main/views.py
+++ app/main/views.py
@@ -6,4 +6,12 @@ from .. import db
 @main.route('/',methods=['GET','POST'])
 def index():
     form = ReviewForm()
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch = form.pitch.data
+        return redirect(url_for('main.new_pitch'))
     return render_template('index.html', review_form=form)
+
+@main.route('/pitches/new_pitch', methods=['GET','POST'])
+def new_pitch():
+    return render_template('new_pitch.html')
