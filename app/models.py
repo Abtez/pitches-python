8e8d3d4bb46d4678f166e66c1f5483e9a626c507
Abzed <ibraabzed@gmail.com>
2020-09-20 07:33:34 +0300

    edit db

--- app/models.py
+++ app/models.py
@@ -8,12 +8,15 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model,UserMixin):
-    __tablename__ = 'Users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255),unique=True,index=True)
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure  = db.Column(db.String(255))
     pitches = db.relationship('Pitch',backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
     
     @property
     def password(self):
